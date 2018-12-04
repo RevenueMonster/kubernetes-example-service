0b103b11fe3384880fc10e73dbd4da714a986061
yussuf <charu.yussuf@gmail.com>
2018-12-05 00:13:52 +0800

    remove pods description

--- main.go
+++ main.go
@@ -2,27 +2,15 @@ package main
 
 import (
 	"os"
-	"time"
 
 	"github.com/labstack/echo"
 )
-
-var podDescription = ""
-
-func init() {
-	podDescription = "Pod started at " + time.Now().UTC().Format("15:04:05")
-}
 
 func main() {
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
-
-		return c.JSON(200, map[string]interface{}{
-			"hostname":    os.Getenv("HOSTNAME"),
-			"message":     os.Getenv("SYSTEM_NAME"),
-			"description": podDescription,
-		})
+		return c.String(200, os.Getenv("SYSTEM_NAME"))
 	})
 
 	e.Logger.Fatal(e.Start(":5000"))
